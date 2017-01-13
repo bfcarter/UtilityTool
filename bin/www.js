64a2294b138ec9805d2b60d383496d929901563a
Brittni Carter <bfcarter@fullsail.edu>
2017-01-12 20:26:27 -0600

    comment that directed port is used

--- bin/www.js
+++ bin/www.js
@@ -3,6 +3,6 @@ var models = require("../models/");
 
 models.sequelize.sync().then(function () {
     app.listen(3000, function () {
-        console.log("Application is listening on port 3000");
+        console.log("Application is listening on port 3000"); //port 3000 used
     });
 });
