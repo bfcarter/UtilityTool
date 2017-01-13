c322b32c0c1930fd4eedb72adba30a5c1b7e32fb
Brittni Carter <bfcarter@fullsail.edu>
2017-01-12 20:19:40 -0600

    randomtext.js corrections and comments

--- controllers/getRandomText.js
+++ controllers/getRandomText.js
@@ -1,14 +1,14 @@
 module.exports = function(length) {
-    if(length < 1) {
+    if(length < 1) { //if greater then 1
         length = 10;
     }
 
     var result = "";
-    var allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; //allowed variables
 
     for(var i=0; i<length; i++) {
         result += allowed.charAt(Math.floor(Math.random() * allowed.length));
     }
 
-    return result;
+    return result; //show answer
 };
